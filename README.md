1441b8af2dc9fc5188616a3c6676074eeb0f607e
Kanza Shahid <90906602+kanza-shahid-dev@users.noreply.github.com>
2023-10-30 11:44:26 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Boilerplate
+
+# TypeScript
+
+# Next Js
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}'
+    }
+  },
+
+  output: 'standalone'
+}
+
+module.exports = nextConfig
